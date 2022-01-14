0359e4503a2a8b0642c4fe706741c81fa26951e2
mbdietrich <max.dietrich@loyalty.co.nz>
2022-01-15 11:21:24 +1300

    Implement comment strip
    Use javaparser to traverse the AST and remove all comments from nodes.
    
    IllegalArgumentException will be raised if the input is not valid Java
    code.
    
    Also splits error handling tests off the parameterized tests.

--- src/main/java/net/mbdietrich/jstrip/CommentStripper.java
+++ src/main/java/net/mbdietrich/jstrip/CommentStripper.java
@@ -1,7 +1,33 @@
 package net.mbdietrich.jstrip;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseProblemException;
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+
 public class CommentStripper {
     public static String strip(String input){
-        throw new java.lang.UnsupportedOperationException("Not Implemented");
+        try {
+            JavaParser javaParser = new JavaParser();
+            ParseResult<CompilationUnit> result = javaParser.parse(input);
+            if(!result.isSuccessful()){
+                throw new IllegalArgumentException("Invalid Java: "+result.getProblems());
+            }
+            CompilationUnit tree = result.getResult().get();
+
+            stripComments(tree);
+
+            return tree.toString();
+        } catch (ParseProblemException e) {
+            throw new IllegalArgumentException("Invalid Java: "+e.getMessage());
+        }
+    }
+
+    // Remove all comment nodes from the AST
+    private static void stripComments(Node tree){
+        for(Node comment : tree.getAllContainedComments()){
+            comment.remove();
+        }
     }
 }
--- src/test/java/net/mbdietrich/jstrip/CommentStripperTest.java
+++ src/test/java/net/mbdietrich/jstrip/CommentStripperTest.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import java.nio.file.Files;
 
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -17,46 +18,68 @@ import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
+@RunWith(Enclosed.class)
 public class CommentStripperTest {
 
-    private final String input;
-    private final String expectedOutput;
+    @RunWith(Parameterized.class)
+    public static class CommentStripperFileAssertions {
 
-    public CommentStripperTest(String input, String expectedOutput) {
-        super();
-        this.input = input;
-        this.expectedOutput = expectedOutput;
-    }
+        private final String input;
+        private final String expectedOutput;
+        private final String testName;
 
-    private static String[] allInputFiles(){
-        File[] inputs = new File("src/test/resources/inputs").listFiles();
-
-        return Stream.of(inputs).map(f -> f.getAbsolutePath().toString()).toArray(String[]::new);
-    }
-
-    private static String outputPathFor(String input){
-        return input.replace("inputs", "outputs");
-    }
-
-    @Parameterized.Parameters
-    public static Collection<String[]> loadFixtures() throws IOException {
-        Collection<String[]> fixtures = new ArrayList<String[]>();
-
-        for(String inputPath : allInputFiles()){
-            String outputPath = outputPathFor(inputPath);
-            String inputStr = Files.readString(Paths.get(inputPath));
-            String outputStr = Files.readString(Paths.get(outputPath));
-
-            fixtures.add(new String[]{inputStr, outputStr});
+        public CommentStripperFileAssertions(String testName, String input, String expectedOutput) {
+            super();
+            this.testName = testName;
+            this.input = input;
+            this.expectedOutput = expectedOutput;
         }
-        return fixtures;
+
+        private static String[] allInputFiles() {
+            File[] inputs = new File("src/test/resources/inputs").listFiles();
+
+            return Stream.of(inputs).map(f -> f.getAbsolutePath().toString()).toArray(String[]::new);
+        }
+
+        private static String outputPathFor(String input) {
+            return input.replace("inputs", "outputs");
+        }
+
+        @Parameterized.Parameters
+        public static Collection<String[]> loadFixtures() throws IOException {
+            Collection<String[]> fixtures = new ArrayList<String[]>();
+
+            for (String inputPath : allInputFiles()) {
+                String testName = new File(inputPath).getName();
+
+                String outputPath = outputPathFor(inputPath);
+                String inputStr = Files.readString(Paths.get(inputPath));
+                String outputStr = Files.readString(Paths.get(outputPath));
+
+                fixtures.add(new String[]{testName, inputStr, outputStr});
+            }
+            return fixtures;
+        }
+
+        @Test
+        public void testTransform() {
+            System.out.println("Testing "+testName);
+
+            String output = CommentStripper.strip(input);
+            assertEquals(expectedOutput, output);
+        }
     }
 
-    @Test
-    public void testTransform() {
-        String output = CommentStripper.strip(input);
-        assertEquals(output, expectedOutput);
-    }
+    public static class CommentStripperFailCases {
+        @Test(expected = IllegalArgumentException.class)
+        public void testInvalidInput() {
+            String input = """
+                        // Invalid java code
+                        public class Invalid {
+                          this shouldn't be here            
+                    """;
 
+            CommentStripper.strip(input);
+        }
+    }
 }
